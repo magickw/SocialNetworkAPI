fa568607b1803af5b8ef99311d3808ad92203a03
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-02-24 16:44:16 -0800

    completed the data.js.

---
+++ utils/data.js
@@ -0,0 +1,93 @@
+const usernames = [
+    'Aaran',
+  'Aaren',
+  'Aarez',
+  'Aarman',
+  'Aaron',
+  'Aaron-James',
+  'Aarron',
+  'Aaryan',
+  'Aaryn',
+  'Aayan',
+  'Aazaan',
+  'Abaan',
+  'Abbas',
+  'Abdallah',
+  'Abdalroof',
+  'Abdihakim',
+  'Abdirahman',
+  'Abdisalam',
+  'Abdul',
+  'Abdul-Aziz',
+  'Abdulbasir',
+  'Abdulkadir',
+  'Abdulkarem',
+  'Smith',
+  'Jones',
+  'Coollastname',
+  'enter_name_here',
+  'Ze',
+  'Zechariah',
+  'Zeek',
+  'Zeeshan',
+  'Zeid',
+  'Zein',
+  'Zen',
+  'Zendel',
+  'Zenith',
+  'Zennon',
+  'Zeph',
+  'Zerah',
+  'Zhen',
+  'Zhi',
+  'Zhong',
+  'Zhuo',
+  'Zi',
+  'Zidane',
+  'Zijie',
+  'Zinedine',
+  'Zion',
+  'Zishan',
+  'Ziya',
+  'Ziyaan',
+  'Zohaib',
+  'Zohair',
+  'Zoubaeir',
+  'Zubair',
+  'Zubayr',
+  'Zuriel',
+  'Xander',
+  'Jared',
+  'Grace',
+  'Alex',
+  'Mark',
+  'Tamar',
+  'Farish',
+  'Sarah',
+  'Nathaniel',
+  'Parker',
+]
+
+const thoughts = [
+    '',
+]
+
+const reactions = [
+    '',
+
+]
+
+ 
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets 
+const getRandomThought = () =>
+  `${getRandomArrItem(thoughts)}`;
+
+const getRandomUserName = () =>
+  `${getRandomArrItem(usernames)}${Math.floor(Math.random() * 10 + 1)}`;
+
+// Export the functions for use in seed.js
+module.exports = { getRandomThought, getRandomUserName };
